eab652431bc9b2fdf1eb00090a39b06a3d696515
xd <xd@xds-MacBook-Pro.local>
2021-01-19 13:16:23 -0500

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,12 @@ def Index():
     """ return message """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     """ return message """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
