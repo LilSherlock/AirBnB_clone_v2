9950ee0eb0ef652d5d6d31e7bac8194e11fd5e30
xd <xd@xds-MacBook-Pro.local>
2021-01-19 14:45:48 -0500

    update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -12,15 +12,18 @@ def Index():
     """ return message """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     """ return message """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def C(text):
     """ return message """
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python/<text>')
 @app.route('/python')
@@ -28,10 +31,12 @@ def python(text='is cool'):
     """ return message """
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>')
 def number(n):
     """ return message """
     return "{:d} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
