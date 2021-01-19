ec77ed0c93a534dbaeb6210b33bd8721453b03b5
xd <xd@xds-MacBook-Pro.local>
2021-01-19 14:44:51 -0500

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -12,21 +12,25 @@ def Index():
     """ return message """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     """ return message """
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def C(text):
     """ return message """
     return 'C ' + text.replace('_', ' ')
 
+
 @app.route('/python/<text>')
 @app.route('/python')
 def python(text='is cool'):
     """ return message """
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
