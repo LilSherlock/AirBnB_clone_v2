a828ef6457c6d97d22bdbf75cbfd4371f86c4e11
xd <xd@xds-MacBook-Pro.local>
2021-01-19 14:39:24 -0500

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,15 +12,18 @@ def Index():
     """ return message """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     """ return message """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def C(text):
     """ return message """
     return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
