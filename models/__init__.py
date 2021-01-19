33bc269214c93982e315aa563b90804c6ba96557
xd <xd@xds-MacBook-Pro.local>
2021-01-19 16:44:15 -0500

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,25 @@
+  
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+'''
+    Package initializer
+'''
 from os import getenv
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
+from models.state import State
+from models.city import City
+from models.user import User
+from models.review import Review
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
+
+
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage
@@ -10,5 +27,4 @@ if storage_type == 'db':
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
